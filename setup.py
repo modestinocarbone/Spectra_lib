f9bc1e0221033678fdcd8d282ba048915845b497
Modestino Carbone <159069824+modestinocarbone@users.noreply.github.com>
2025-09-11 10:15:36 +0200

    Setup installation file [FIRST COMMIT]

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 10 17:23:32 2025
+
+@author: Modestino Carbone
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Spectra_lib",
+    version="2.0",
+    author="Modestino Carbone",
+    description="A collection of Python spectrum function for Harmonic analysis",
+    packages=find_packages(),
+)
